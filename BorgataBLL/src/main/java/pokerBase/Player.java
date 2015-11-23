27d99a9a182357e383bb995c4bcbd39f3d60c9e1
Mandy Morris <mjmorris@udel.edu>
2015-11-23 13:59:30 -0500

    Initial Commit

---
+++ BorgataBLL/src/main/java/pokerBase/Player.java
@@ -0,0 +1,16 @@
+package pokerBase;
+
+import domain.PlayerDomainModel;
+
+public class Player extends PlayerDomainModel {
+
+	public Player(String PlayerName, int iPlayerPosition) {
+		super(PlayerName, iPlayerPosition);
+	}
+
+	public int getiPlayerPosition() {
+		// TODO Auto-generated method stub
+		return iPlayerPosition;
+	}
+
+}
